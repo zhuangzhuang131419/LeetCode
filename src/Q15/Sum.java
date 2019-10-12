bed0eed3bd8407cb14571e811c7d59c437791cd3
zhuangzhuang <chichengzheng1314@gmail.com>
2019-10-11 20:19:45 -0400

    Q15 done

--- src/Q15/Sum.java
+++ src/Q15/Sum.java
@@ -1,45 +1,42 @@
 package Q15;
 
-/**
+/*
  * Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0?
  * Find all unique triplets in the array which gives the sum of zero.
  */
 
-import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Sum {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> l = new LinkedList<>();
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                for (int k = j + 1; k < nums.length; k++) {
-
-                    if (nums[i] + nums[j] + nums[k] == 0) {
-                        List<Integer> list = new LinkedList<>() {
-                            @Override
-                            public boolean equals(Object o) {
-                                if (o instanceof List) {
-                                    return (((List) o).get(0) == this.get(0) && ((List) o).get(1) == this.get(1) && ((List) o).get(2) == this.get(2))
-                                            || (((List) o).get(0) == this.get(0) && ((List) o).get(1) == this.get(2) && ((List) o).get(2) == this.get(1))
-                                            || (((List) o).get(0) == this.get(1) && ((List) o).get(1) == this.get(0) && ((List) o).get(2) == this.get(2))
-                                            || (((List) o).get(0) == this.get(1) && ((List) o).get(1) == this.get(2) && ((List) o).get(2) == this.get(0))
-                                            || (((List) o).get(0) == this.get(2) && ((List) o).get(1) == this.get(1) && ((List) o).get(2) == this.get(0))
-                                            || (((List) o).get(0) == this.get(2) && ((List) o).get(1) == this.get(0) && ((List) o).get(2) == this.get(1));
-                                }
-                                return false;
-                            }
-                        };
-                        list.add(nums[i]);
-                        list.add(nums[j]);
-                        list.add(nums[k]);
-                        if (!l.contains(list)) {
-                            l.add(list);
-                        }
-                    }
+        List<List<Integer>> triplets = new ArrayList<>();
+        int len = nums.length;
+        Arrays.sort(nums);
+        for (int i = 0; i < len - 2; i++) {
+            if (i > 0)
+                while (i < len - 2 && nums[i - 1] == nums[i])
+                    i++; // to exclude duplicates
+            int j = i + 1;
+            int k = len - 1;
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == 0) {
+                    triplets.add(new ArrayList<>(Arrays.asList(nums[i], nums[j], nums[k])));
+                    j++;
+                    k--;
+                    while ((j < k) && (nums[j - 1] == nums[j])) // to exclude duplicates
+                        j++;
+                    while ((j < k) && (nums[k] == nums[k + 1])) // to exclude duplicates
+                        k--;
+                } else if (sum < 0) {
+                    j++; // sum too low
+                } else {
+                    k--; // sum too high
                 }
             }
         }
-        return l;
+        return triplets;
     }
 }
