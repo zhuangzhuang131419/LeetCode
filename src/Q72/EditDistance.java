390a7413f9c0cafac585c6b2bae46d259d57738b
Chicheng <zy22092@nottingham.edu.cn>
2019-10-14 14:07:48 -0400

    Q72 done

---
+++ src/Q72/EditDistance.java
@@ -0,0 +1,37 @@
+package Q72;
+
+/**
+ * Given two words word1 and word2, find the minimum number of operations required to convert word1 to word2.
+ *
+ * You have the following 3 operations permitted on a word:
+ *
+ * Insert a character
+ * Delete a character
+ * Replace a character
+ */
+
+public class EditDistance {
+    public int minDistance(String word1, String word2) {
+        int[][] dp = new int[word1.length() + 1][word2.length() + 1];
+        for (int i = 0; i < word1.length(); i++) {
+            dp[i + 1][0] = dp[i][0] + 1;
+        }
+
+        for (int i = 0; i < word2.length(); i++) {
+            dp[0][i + 1] = dp[0][i] + 1;
+        }
+
+        for (int i = 0; i < word1.length(); i++) {
+            for (int j = 0; j < word2.length(); j++) {
+                if (word1.charAt(i) == word2.charAt(j)) {
+                    dp[i + 1][j + 1] = dp[i][j];
+                } else {
+                    dp[i + 1][j + 1] = Math.min(Math.min(dp[i][j + 1], dp[i + 1][j]), dp[i][j]) + 1;
+                }
+            }
+        }
+        
+        return dp[word1.length()][word2.length()];
+    }
+
+}
