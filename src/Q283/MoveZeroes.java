fd8b5621b8c87290b294191cac81f9e1c1e0faac
zhuangzhuang <chichengzheng1314@gmail.com>
2019-10-20 23:01:04 -0400

    Q283 done

---
+++ src/Q283/MoveZeroes.java
@@ -0,0 +1,16 @@
+package Q283;
+
+public class MoveZeroes {
+    public void moveZeroes(int[] nums) {
+        if (nums.length == 0) return;
+        int i = 0;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != 0) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                i++;
+            }
+        }
+    }
+}
